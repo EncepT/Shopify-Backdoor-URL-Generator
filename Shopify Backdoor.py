f87ba77afc4c224e63b651ab7592f31e7b057e66
EncepT <enceept@gmail.com>
2017-01-30 18:42:22 +0100

    Update Shopify Backdoor.py

--- Shopify Backdoor.py
+++ Shopify Backdoor.py
@@ -5,6 +5,9 @@
 __author__ = 'EncepT // Shiba'
 import time
 import pyperclip
+import webbrowser
+import sys
+import subprocess
 
 def URLGEN(site, itemid):
     URL = str(site) + "/cart/add/" + str(Itemid)
@@ -17,9 +20,10 @@ URL = URLGEN(site, Itemid)
 
 print (str(site) , "Backdoor link Created..")
 time.sleep(1)
-print (URL)
+print ("Opening your default browser...")
 time.sleep(1)
-print ("Copied to Clipboard")
-pyperclip.copy(URL)
-time.sleep(1)
-print ("Enjoy your Pair ;)")
+if sys.platform == 'darwin':    # in case of OS X
+    subprocess.Popen(['open', URL])
+else:
+    webbrowser.open_new_tab(URL)
+print("Enjoy ;)")
